52da4906ae29bc828ece22850b99cf3b28f7bd61
Jens-Alrik Adrian <jens-alrik.adrian@gmx.de>
2017-05-04 22:57:45 +0200

    Include the function and a tester

---
+++ robustNonlinLsq_test.m
@@ -0,0 +1,45 @@
+function [tests] = robustNonlinLsq_test()
+%ROBUSTNONLINLSQ_TEST <purpose in one line!>
+%
+% Usage: [tests] = robustNonlinLsq_test()
+%
+%
+% Author:  J.-A. Adrian (JA) <jensalrik.adrian AT gmail.com>
+% Date  :  04-May-2017 22:54
+%
+
+% History:  v0.1  initial version, 04-May-2017 (JA)
+%
+
+
+tests = functiontests(localfunctions());
+end
+
+
+function setup(testCase)
+
+end
+
+function teardown(testCase)
+
+end
+
+function setupOnce(testCase)
+
+end
+
+function teardownOnce(testCase)
+
+end
+
+
+function testSomething(testCase)
+
+end
+
+function awesomeTest(testCase)
+
+end
+
+
+% End of file: robustNonlinLsq_test.m
