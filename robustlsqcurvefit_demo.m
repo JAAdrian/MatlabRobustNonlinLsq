cfcef0d50862b0887791e82ec7d252eace75901e
Jens-Alrik Adrian <jens-alrik.adrian@gmx.de>
2017-05-07 15:26:54 +0200

    Update demo

--- robustlsqcurvefit_demo.m
+++ robustlsqcurvefit_demo.m
@@ -21,16 +21,16 @@ x0       = [-1, 1];
 options = optimset(@lsqcurvefit);
 options.Display = 'off';
 
-estParamsReference = polyfit(x, y, 1);
-estParams          = robustlsqcurvefit(modelFun, x0, x, y, [], [], [], options);
-estRobustFit       = robustfit(x, y);
+estParamsOrdinary  = polyfit(x, y, 1);
+estParamsRobust    = robustlsqcurvefit(modelFun, x0, x, y, [], [], [], options);
+estLinearRobustFit = robustfit(x, y);
 
 figure;
 hold on;
 scatter(x, y, 'filled');
 plot(x, modelFun([-2, 10], x), 'k--', 'linewidth', 2);
-plot(x, modelFun(estParamsReference, x), 'linewidth', 2);
-plot(x, modelFun(estParams, x), 'linewidth', 2);
+plot(x, modelFun(estParamsOrdinary, x), 'linewidth', 2);
+plot(x, modelFun(estParamsRobust, x), 'linewidth', 2);
 hold off;
 grid on;
 
@@ -58,15 +58,15 @@ x0 = [0.3, 2];
 options = optimset(@lsqcurvefit);
 options.Display = 'off';
 
-estParamsReference = lsqcurvefit(modelFun, x0, x, y, [], [], options);
-estParams          = robustlsqcurvefit(modelFun, x0, x, y, [], [], [], options);
+estParamsOrdinary = lsqcurvefit(modelFun, x0, x, y, [], [], options);
+estParamsRobust   = robustlsqcurvefit(modelFun, x0, x, y, [], [], [], options);
 
 figure;
 hold on;
 scatter(x, y, 'filled');
 plot(x, modelFun(trueParams, x), 'k--', 'linewidth', 2);
-plot(x, modelFun(estParamsReference, x), 'linewidth', 2);
-plot(x, modelFun(estParams, x), 'linewidth', 2);
+plot(x, modelFun(estParamsOrdinary, x), 'linewidth', 2);
+plot(x, modelFun(estParamsRobust, x), 'linewidth', 2);
 hold off;
 grid on;
 
@@ -76,12 +76,14 @@ legend(...
     );
 
 %% Square-Root Function Fit incl. Lower and Upper Parameter Bounds
+% the model's true parameters
 trueParams = [50, -0.7];
 
+% create some noisy data using a square root function with 2 parameters and nasty noise
 x = linspace(0, 20, 100);
 modelFun = @(param, x) param(1)*sqrt(max(0, x + param(2)));
 
-noise = sin(x).^2 .* randn(size(x));
+noise = 10*sin(x).^2 .* randn(size(x));
 
 y = modelFun(trueParams, x) + noise;
 
@@ -89,30 +91,39 @@ y = modelFun(trueParams, x) + noise;
 y(11)  = -300;
 y(end) = 2000;
 
+% make sure the parameters are within the following range
 lb = [1, -inf];
 ub = [inf, 10];
 
+% initial guess
 x0 = [1, -1];
 
+% don't show infos from lsqcurvefit()
 options = optimset(@lsqcurvefit);
 options.Display = 'off';
 
-estParamsReference = lsqcurvefit(modelFun, x0, x, y, lb, ub, options);
-estParams          = robustlsqcurvefit(modelFun, x0, x, y, lb, ub, [], options);
+% estimate parameters using ordinary and robust LSQ
+estParamsOrdinary = lsqcurvefit(modelFun, x0, x, y, lb, ub, options);
+estParamsRobust   = robustlsqcurvefit(modelFun, x0, x, y, lb, ub, [], options);
 
 figure;
 hold on;
 scatter(x, y, 'filled');
 plot(x, modelFun(trueParams, x), 'k--', 'linewidth', 2);
-plot(x, modelFun(estParamsReference, x), 'linewidth', 2');
-plot(x, modelFun(estParams, x), 'linewidth', 2);
+plot(x, modelFun(estParamsOrdinary, x), 'linewidth', 2');
+plot(x, modelFun(estParamsRobust, x), 'linewidth', 2);
 hold off;
 grid on;
+
+title('Square-Root Fit');
+xlabel('x data');
+ylabel('y data');
 
 legend(...
     {'Noisy Data', 'True Regression', 'Ordinary non-lin LSQ', 'Robust non-lin LSQ'}, ...
     'fontsize', 12 ...
     );
+
 
 
 % End of file: robustlsqcurvefit_demo.m
